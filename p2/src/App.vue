d153f4879bdc615f32b89b695b9dc98fbf0fa32f
SKS IT FIRM <sksitfirm@gmail.com>
2023-08-01 09:42:56 +0600

    m3 > 04-importing-html-markup-in-vue-project

--- p2/src/App.vue
+++ p2/src/App.vue
@@ -3,7 +3,10 @@
 </script>
 
 <template>
-  
+  <div class="container mx-auto flex items-center justify-center min-h-screen flex-col">
+    <h1 class="text-4xl">Welcome. Get ready to master Vue.js 3!</h1>
+    <h2 class="mt-10 text-xl text-white-700">Today <span>1st August, 2023</span></h2>
+  </div>
 </template>
 
 <style scoped>
