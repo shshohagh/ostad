4282a7e41ba17f8351565fbe5c1f9a7be7bc2ba1
SKS IT FIRM <sksitfirm@gmail.com>
2023-08-02 00:03:03 +0600

    m3 > 08-binding-data-with-html-attribute

--- p2/src/App.vue
+++ p2/src/App.vue
@@ -1,15 +1,53 @@
 <script setup>
-// import {data} from './data/data'
-import data from './data/data'
+const people = [
+    {
+        image:'https://images.generated.photos/zRuly38LuHo-upVQfxZjUb6YXOHXapy58gXXFglv1Rg/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MDAzMzE4LmpwZw.jpg',
+        name: 'Jane Doe',
+        designation: 'CEO, Doe Corporation',
+        location: '968 Ocala Street, Orlando, Florida'
+    },
+    {
+        image:'https://images.generated.photos/dVrq4AXS8SmlQ_HTyxG4bJRmi4IulUA4omUbvvSyQMU/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NDkwODEzLmpwZw.jpg',
+        name: 'John Doe',
+        designation: 'CTO, Doe Corporation',
+        location: '2970 Duke Lane, Brunswick, New Jersey'
+    },
+    {
+        image:'https://images.generated.photos/XkTVulI9UnbMVl1veOfrra6bS2v0QIqLbSIQrgXni14/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MTYxODM1LmpwZw.jpg',
+        name: 'Jimmy Doe',
+        designation: 'CMO, Doe Corporation',
+        location: '3679 Elk Street, Denver, Colorado'
+    }
+]
 </script>
 
 <template>
-  <div class="container mx-auto flex items-center justify-center min-h-screen flex-col">
-    <h1 class="text-4xl">{{ data.message }}</h1>
-    <h2 class="mt-10 text-xl text-white-700">Today is <span>{{ data.date }}</span></h2>
-    <h3 class="mt-10 text-xl text-white-700">You have <span>{{ data.tasks.length }}</span> tasks for today</h3>
-    <h3 class="mt-10 text-xl text-white-700">You have <span>{{ data.tasks[0] }}</span> tasks for today</h3>
-  </div>
+  <section>
+    <h1 style="text-align:center;margin:40px 0px;font-size:40px">Data Example</h1>
+    <section class="content">
+        <section class="person"><img :src="people[0].image" alt="Jane Doe">
+            <div>
+                <h1>{{ people[0].name }}</h1>
+                <h2>{{ people[0].designation }}</h2>
+                <p> {{ people[0].location }}</p>
+            </div>
+        </section>
+        <section class="person"><img :src="people[1].image" alt="Jane Doe">
+            <div>
+                <h1>{{ people[1].name }}</h1>
+                <h2>{{ people[1].designation }}</h2>
+                <p> {{ people[1].location }}</p>
+            </div>
+        </section>
+        <section class="person"><img :src="people[2].image" alt="Jane Doe">
+            <div>
+                <h1>{{ people[2].name }}</h1>
+                <h2>{{ people[2].designation }}</h2>
+                <p> {{ people[2].location }}</p>
+            </div>
+        </section>
+    </section>
+  </section>
 </template>
 
 <style scoped>
