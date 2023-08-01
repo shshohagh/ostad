96a35b0659d2ac815941543176958bae54e35da4
SKS IT FIRM <sksitfirm@gmail.com>
2023-08-01 22:51:28 +0600

    m3 > 06-display-data-from-object-and-array

--- p2/src/App.vue
+++ p2/src/App.vue
@@ -5,12 +5,19 @@ function getDate(){
   const date = new Date()
   return date.toDateString()
 }
+const data = {
+  message:'Welcome. Get ready to master Vue.js 3! - Obj',
+  date:getDate()+' -Obj',
+  tasks:['"No"',1,2,3,4,5,6,7]
+}
 </script>
 
 <template>
   <div class="container mx-auto flex items-center justify-center min-h-screen flex-col">
-    <h1 class="text-4xl">{{ message }}</h1>
-    <h2 class="mt-10 text-xl text-white-700">Today is <span>{{ getDate() }}</span></h2>
+    <h1 class="text-4xl">{{ data.message }}</h1>
+    <h2 class="mt-10 text-xl text-white-700">Today is <span>{{ data.date }}</span></h2>
+    <h3 class="mt-10 text-xl text-white-700">You have <span>{{ data.tasks.length }}</span> tasks for today</h3>
+    <h3 class="mt-10 text-xl text-white-700">You have <span>{{ data.tasks[0] }}</span> tasks for today</h3>
   </div>
 </template>
 
