69f21513ec364a887f3f91978523d485d66fba95
SKS IT FIRM <sksitfirm@gmail.com>
2023-08-01 22:04:15 +0600

    m3 > 05-using-data-in-vuejs

--- p2/src/App.vue
+++ p2/src/App.vue
@@ -1,11 +1,16 @@
 <script setup>
-
+const message= 'Welcome. Get ready to master Vue.js 3!'
+const date = '1st August, 2023 '
+function getDate(){
+  const date = new Date()
+  return date.toDateString()
+}
 </script>
 
 <template>
   <div class="container mx-auto flex items-center justify-center min-h-screen flex-col">
-    <h1 class="text-4xl">Welcome. Get ready to master Vue.js 3!</h1>
-    <h2 class="mt-10 text-xl text-white-700">Today <span>1st August, 2023</span></h2>
+    <h1 class="text-4xl">{{ message }}</h1>
+    <h2 class="mt-10 text-xl text-white-700">Today is <span>{{ getDate() }}</span></h2>
   </div>
 </template>
 
