e8ac0a12f01bac6c2bfe067e3b836a5b205a8113
SKS IT FIRM <sksitfirm@gmail.com>
2023-09-06 10:51:29 +0600

    Update App.vue

--- p2/src/App.vue
+++ p2/src/App.vue
@@ -7,18 +7,17 @@ const status_size = 'text-2xl'
 const bgOrange="bg-orange-400"
 const bgIndigo="bg-indigo-400"
 const bgGreen="bg-green-400"
-const weekday = true
+const weekday = false
 const colors = {
-    bgColor:'bg-teal-100',
-    borderColor:'border-teal-500',
-    textColor:'text-teal-900'
+    bgColor:'bg-blue-100',
+    borderColor:'border-blue-500',
+    textColor:'text-blue-900'
 }
 const messageType = 'error'
-if('error'==messageType){
-    colors.bgColor='bg-red-100',
-    colors.borderColor='border-red-500',
-    colors.textColor='text-red-900'
-}
+if('success' == messageType){ colors.bgColor='bg-teal-100', colors.borderColor='border-teal-500', colors.textColor='text-teal-900' }
+if('error' == messageType){ colors.bgColor='bg-red-100', colors.borderColor='border-red-500', colors.textColor='text-red-900' }
+if('warning' == messageType){ colors.bgColor='bg-yellow-100', colors.borderColor='border-yellow-500', colors.textColor='text-yellow-900' }
+
 </script>
 
 <template>
@@ -30,7 +29,9 @@ if('error'==messageType){
             <div class="w-1/3 h-16 p-5" :class="bgGreen">Green</div>
         </section>
         <section class="mt-10">
-            <div class="h-16 flex items-center justify-center text-white p-3" :class="[weekday?'bg-red-400':'bg-green-400',weekday==true?'text-2xl':'text-4xl']">It's a Weekday!</div>
+            <div class="h-16 flex items-center justify-center text-white p-3" :class="[weekday?'bg-red-400':'bg-green-400',weekday==true?'text-2xl':'text-4xl']"> 
+                {{weekday?"It's a Weekday":"It's a Weekend!"}}
+            </div>
         </section>
         <section class="mt-10">
             <div class="border-t-4 round-b py-3 shadow-md" :class="[colors.bgColor, colors.borderColor, colors.textColor]">
