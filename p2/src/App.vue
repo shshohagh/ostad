39a95277e1aa85c705a44d753cbdc0c4feba3d94
SKS IT FIRM <sksitfirm@gmail.com>
2023-09-06 08:53:49 +0600

    m4 > CSS Binding

--- p2/src/App.vue
+++ p2/src/App.vue
@@ -2,6 +2,7 @@
 import {ref} from 'vue'
 const location = 'Dhaka' 
 const weather  = ref("s") 
+const status_size = 'text-2xl'
 </script>
 
 <template>
@@ -11,20 +12,20 @@ const weather  = ref("s")
         <div class="w-1/3 bg-black-200 p-10 flex items-center flex-col space-y-10">
             <h1 class="text-3xl">Dhaka</h1>
             <template v-if="weather=='s'">
-                <h2 class="text-2xl">Weather: Sunney</h2>
+                <h2 :class="status_size">Weather: Sunney</h2>
                 <img id="sunney" src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7911203/weather-icon-md.png" alt="">
             </template>
             <template v-if="weather=='pc'">
-                <h2 class="text-2xl">Weather: Partly Cloudy</h2>
+                <h2 :class="status_size">Weather: Partly Cloudy</h2>
                 <img src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7912718/weather-icon-md.png" alt="">
             </template>
             <template v-if="weather=='r'">
-                <h2 class="text-2xl">Weather: Rainy</h2>
+                <h2 :class="status_size">Weather: Rainy</h2>
                 <img src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7913380/weather-icon-md.png" alt="">
 
             </template>
             <template v-if="weather=='t'">
-                <h2 class="text-2xl">Weather: Thunderstrom</h2>
+                <h2 :class="status_size">Weather: Thunderstrom</h2>
                 <img src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7912589/weather-icon-md.png" alt="">
             </template>
 
