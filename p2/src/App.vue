0cd50120c118e5595eb47c98a9efa78c8f28f560
SKS IT FIRM <sksitfirm@gmail.com>
2023-09-07 08:34:13 +0600

    episode 02 - display index in loop

--- p2/src/App.vue
+++ p2/src/App.vue
@@ -1,16 +1,20 @@
 <script setup>
-const items = [1,2,3,4,5]
+const status = [
+    "Pending",
+    "Picked Up",
+    "Processing",
+    "On The Way",
+];
 </script>
 
 <template>
 <section class="container mx-auto">
-    <h1 class="text-center text-3xl py-5"> List Rendering</h1>
-        <section class="mt-1">
-            <div class="h-16 flex items-center justify-center text-white p-3 mx-12 my-1 border border-indigo-400" v-for="item in items" > {{ item }} </div>
 
-            <div class="h-16 flex items-center justify-center text-white p-3 mx-12 my-1 border border-orange-400" v-for="n in 5" > {{ n }} </div>
+    <h1 class="text-center text-3xl py-5"> episode 02 - display index in loop</h1>
+        <section class="mt-1">
+            <div class="h-16 flex items-center justify-center text-white p-3 mx-12 my-1 border border-indigo-400" v-for="(stat,index) in status" > {{ index }}, {{ stat }} </div>
         </section>
-        
+
 </section>
 </template>
 
