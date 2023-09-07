ed865ad00de70ac989da2ed4638624d8c4710b2a
SKS IT FIRM <sksitfirm@gmail.com>
2023-09-07 08:37:10 +0600

    Update App.vue

--- p2/src/App.vue
+++ p2/src/App.vue
@@ -12,7 +12,7 @@ const status = [
 
     <h1 class="text-center text-3xl py-5"> episode 02 - display index in loop</h1>
         <section class="mt-1">
-            <div class="h-16 flex items-center justify-center text-white p-3 mx-12 my-1 border border-indigo-400" v-for="(stat,index) in status" > {{ index }}, {{ stat }} </div>
+            <div class="h-16 flex items-center justify-center text-white p-3 mx-12 my-1 border border-indigo-400" v-for="(stat,index) in status" > {{ index+1 }}, {{ stat }} </div>
         </section>
 
 </section>
